6a6b6eba11645aae76767a2c1373f1775b1bd1b7
Kamil Ruchaj <kamilruchaj@gmail.com>
2021-11-29 23:59:32 +0100

    Still working...

--- main.py
+++ main.py
@@ -1,77 +1,67 @@
+TESTING_INSTRUCTION = """RUU
+DDDL
+URLLL"""
 
-
-# TODO 10: Delete "print"
-# TODO 10: Delete "print"
+LUNAR_INSTRUCTION = """RLRLLLULULULUUDUULULRDDLURURDDLDUUDDLRDDUUUDD
+LDLRLDDDLUDRDRRUDUURLRULLUDDRLURLUULDLLRLLUDLRLRUDLULRLRRL
+LLRRDURRDLDULRDUDLRDRDRURULDU
+DULRRDRLRLUDLLURURLLRLRDLLDLLDRDUURL
+DUULULUUDUDLLRLRURULLDLRRLURDLLDUDUDDRURRLUDULULD"""
 
 # TODO 9: Make descriptions of what you are doing!!
-class Move:
+
+class PinPAD:
     def __init__(self):
+        # Starting position on PIN pad
         self.position = [1, 1]
-        # TODO 1: make matrix
 
-        # TODO 9: Make descriptions of what you are doing!!
+        # Generating matrix - PIN pad
+        self.matrix = []
 
-        self.matrix_2 = []
+        number = 0
+        number_of_rows = 3
+        number_of_column = 3
 
-        start_number = 0
-        row = 3
-        column = 3
+        for row in range(number_of_rows):
+            x = []
 
-        for a in range(row):
-            row = []
+            for column in range(number_of_column):
+                number += 1
+                x.append(number)
 
-            for a in range(column):
-                start_number += 1
-                row.append(start_number)
+            self.matrix.append(x)
 
-            print(row)
-            self.matrix_2.append(row)
-
-        print(self.matrix_2)
-
-    # TODO 9: Make descriptions of what you are doing!!
-    def boundary_condition(function):
+    # Decoration making boundary condition
+    def boundary_conditions(function):
         def boundary_condition_wrapper(self):
-            # print(f"1. Position before run function: {test.position}")
-            # TODO 8: What is wrong??
             function(self)
-            # print(f"2. Position after run function: {test.position}")
-            print(self.position)
-
-
-
             for self.xy in self.position:
-
                 if self.xy >= 3:
                     self.position[self.position.index(self.xy)] = 2
-
                 elif self.xy <= 0:
                     self.position[self.position.index(self.xy)] = 0
-
-            # print(f"3. Position after all function: {test.position}")
-
             return self.position
 
         return boundary_condition_wrapper
 
-    # TODO 9: Make descriptions of what you are doing!!
-    def number_in_matrix(self):
-        return self.matrix_2[self.position[0]][self.position[1]]
+    # Getting number from Pin PAD/matrix
+    def number_on_pin_pad(self):
+        return self.matrix[self.position[0]][self.position[1]]
 
-    # TODO 9: Make descriptions of what you are doing!!
-    @boundary_condition
+    # Functions of movements on Pin PAD
+    @boundary_conditions
     def up(self):
         self.position[0] -= 1
 
-    @boundary_condition
+    @boundary_conditions
     def down(self):
         self.position[0] += 1
 
-    @boundary_condition
+    @boundary_conditions
     def right(self):
         self.position[1] += 1
 
-    @boundary_condition
+    @boundary_conditions
     def left(self):
         self.position[1] -= 1
 
@@ -86,51 +76,23 @@ class Move:
             self.left()
 
 
-
-
 # TODO 9: Make descriptions of what you are doing!!
-
-# TODO 8: Input (test)
 
 # TODO 4: Write function what reads input and translates it (ease one)
 
 # TODO 11: Write function what reads input and translates it (HARD ONE)
 
-# TODO 5: Use all function to create working program.
-
-from_user_instrukction = input("Instruction are:\n")
-
-instructions_1 = """LLLLLLLLLDDDDDRRRRRRR"""
-
-instructions_ = """RUU
-DDDL
-URLLL"""
-
-instructions ="""RLRLLLULULULUUDUULULRDDLURURDDLDUUDDLRDDUUUDD
-LDLRLDDDLUDRDRRUDUURLRULLUDDRLURLUULDLLRLLUDLRLRUDLULRLRRL
-LLRRDURRDLDULRDUDLRDRDRURULDU
-DULRRDRLRLUDLLURURLLRLRDLLDLLDRDUURL
-DUULULUUDUDLLRLRURULLDLRRLURDLLDUDUDDRURRLUDULULD"""
-
-test = Move()
+pin_pad = PinPAD()
 pin = ""
 
-
-instructions = instructions.split("\n")
+# TODO: Comment
+instructions = LUNAR_INSTRUCTION.split("\n")
 for row in instructions:
     for letter in row:
-        # print(f"Actual letter: {letter},\nPosition: {test.position},\nNumber: {test.number_in_matrix()}")
-        test.move(input_move=letter)
-        # print(f"Actual letter: {letter},\nPosition: {test.position},\nNumber: {test.number_in_matrix()}")
+        pin_pad.move(input_move=letter)
+    pin += str(pin_pad.number_on_pin_pad())
+    pin_pad.starting_position = pin_pad.position
 
-    pin += str(test.number_in_matrix())
-    test.starting_position = test.position
+print(f"PIN: {pin}\nEnding position on PIN_Pad: {pin_pad.position}")
 
-
-print(pin)
-
-
-print(f"4. Ending position: {test.position}")
-
-
-test.number_in_matrix()
+pin_pad.number_on_pin_pad()
